e8606abbd255702df30c7643bb60f3f6aee444b9
André Luís Alves <prof.andreluis.info@gmail.com>
2026-04-15 20:41:45 -0300

    15042026_2041

--- resources/views/guest/home/quick-access.blade.php
+++ resources/views/guest/home/quick-access.blade.php
@@ -19,7 +19,7 @@
                             </a>
                         @endif
                             <a href="#courses" target="_blank">
-                                <i class="bi bi-mortarboard fs-1"></i> Cursos EITAAAA
+                                <i class="bi bi-mortarboard fs-1"></i> Cursos
                             </a>
                         @if ($calendar->isInscriptionOpen())
                             <a href="#" data-bs-toggle="modal" data-bs-target="#register">
