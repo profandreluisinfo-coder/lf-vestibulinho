1fc14a5b23e830f6bbc84d6725d58a006bed4efb
André Luís Alves <prof.andreluis.info@gmail.com>
2026-08-19 18:54:10 -0300

    19082026_1854

--- resources/views/emails/verify/token.blade.php
+++ resources/views/emails/verify/token.blade.php
@@ -6,7 +6,7 @@
 
 <p>Para dar continuidade ao processo, acesse a Área do Candidato com o e-mail e a senha cadastrados e preencha integralmente o formulário de inscrição disponível.</p>
 
-<p><strong>Atenção:</strong> Sua participação no Vestibulinho 2027 será efetivada <strong>somente após</strong> a conclusão dessa etapa.</p>
+<p><strong>Atenção:</strong> Sua participação no Vestibulinho {{ $process?->year}} será efetivada <strong>somente após</strong> a conclusão dessa etapa.</p>
 
 <p>Desejamos a você êxito em sua trajetória!</p>
 
