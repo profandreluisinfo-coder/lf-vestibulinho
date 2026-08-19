213269e6529bc3793a938dc46ba4a92532585078
André Luís Alves <prof.andreluis.info@gmail.com>
2026-08-19 18:50:54 -0300

    19082026_1850

--- resources/views/emails/verify/token.blade.php
+++ resources/views/emails/verify/token.blade.php
@@ -1,9 +1,17 @@
-<p>Prezado(a) {{ $name }}:</p>
-<p><strong>Seu endereço de e-mail foi confirmado com sucesso!</strong></p>
-<p>Este procedimento APENAS garante seu acesso à <strong>Área do Candidato</strong>. O que significa que você ainda <strong>NÃO está inscrito</strong> no Vestibulinho.</p>
-<p>Para acessá-la, informe o e-mail e senha cadastrados e preencha o <strong>formulário de inscrição</strong>.</p>
-<p><b>IMPORTANTE:</b>Somente após este procedimento, você estará apto para participar do Vestibulinho {{ $process?->year }}.</p>
-<p>Desejamos boa sorte!</p>
-<p>Até breve,</p>
-<p><strong>E. M. Dr. Leandro Franceschini - Prefeitura Muninicipal de Sumaré</strong></p>
+<p>Prezado(a) Candidato(a),</p>
+
+<p>Confirmamos o sucesso na validação do seu endereço de e-mail!</p>
+
+<p>Ressaltamos que esta etapa garante <strong>exclusivamente</strong> o acesso à Área do Candidato, <strong>não significando</strong>, portanto, sua inscrição efetiva no Vestibulinho.</p>
+
+<p>Para dar continuidade ao processo, acesse a Área do Candidato com o e-mail e a senha cadastrados e preencha integralmente o formulário de inscrição disponível.</p>
+
+<p><strong>Atenção:</strong> Sua participação no Vestibulinho 2027 será efetivada <strong>somente após</strong> a conclusão dessa etapa.</p>
+
+<p>Desejamos a você êxito em sua trajetória!</p>
+
+<p>
+  <strong>Atenciosamente,</strong><br>
+  E. M. Dr. Leandro Franceschini - Prefeitura Municipal de Sumaré
+</p>
 @include("partials.emails.footer")
--- resources/views/partials/emails/footer.blade.php
+++ resources/views/partials/emails/footer.blade.php
@@ -1,6 +1,18 @@
 <hr>
-<p><small>Esta mensagem foi enviada de um endereço de e-mail exclusivo para notificações que não recebe e-mails. Portanto <strong>NÃO</strong> responda a esta mensagem.</small></p>
+
+<p><small>
+  Esta mensagem foi enviada de um endereço de e-mail exclusivo para notificações que não recebe e-mails. 
+  Portanto, <strong>NÃO</strong> responda a esta mensagem.
+</small></p>
+
 <p style="font-size: 0.9em; color: #555;">
-Se você não reconhece esta solicitação, por favor, desconsidere este e-mail ou entre em contato conosco.
+  Se você não reconhece esta solicitação, por favor, desconsidere este e-mail ou entre em contato conosco.
 </p>
-<p>Enviado por <strong>Escola Municipal Dr. Leandro Franceschini - Sumaré - SP</strong></p>
+
+<p>
+  <strong>Escola Municipal Dr. Leandro Franceschini - Sumaré - SP</strong><br>
+  R. Geraldo de Souza, 157/221 - Jardim São Carlos<br>
+  Sumaré - SP - CEP: 13170-232<br>
+  Telefone: (19) 3873-2605<br>
+  Horário de atendimento: 18h30 – 22h30
+</p>
